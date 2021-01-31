1d7270bb5c5a8041deb4dfb51af7f7be9737694a
Martin Jonas <martin.jonas@growjob.com>
2021-01-31 10:39:39 +0100

    Fixed exceptions autoloading

--- src/MetisFW/Vimeo/Exceptions/InvalidPlayerParameterException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MetisFW\Vimeo;
-
-class InvalidPlayerParameterException extends VimeoException {
-
-}
---
+++ src/MetisFW/Vimeo/exceptions.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MetisFW\Vimeo;
+
+class InvalidArgumentException extends \InvalidArgumentException {}
+
+class VimeoException extends \RuntimeException {}
+
+class InvalidPlayerParameterException extends VimeoException {}
